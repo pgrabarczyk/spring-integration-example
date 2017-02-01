68f3d12dd6ac4076d76ed3d99724c56be7b66ae2
Patryk Grabarczyk <grabarczyk.patryk@gmail.com>
2017-02-01 16:05:29 +0100

    Added integration-stdin-stdout

--- src/main/java/com/pgrabarczyk/spring/integration/example/Main.java
+++ src/main/java/com/pgrabarczyk/spring/integration/example/Main.java
@@ -1,10 +1,15 @@
 package com.pgrabarczyk.spring.integration.example;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-	log.debug("Hello World");
+
+	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+		"/spring/integration-stdin-stdout.xml");
+
     }
 }
